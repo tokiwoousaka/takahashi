03e98aeb062544358f8a6c4291f1a8bd88df17e8
Tokiwo Ousaka <its.out.of.tune.this.my.music@gmail.com>
2015-05-19 23:40:37 +0900

    Edit lens prism material

--- src/20150530LensPrism.hs
+++ src/20150530LensPrism.hs
@@ -14,7 +14,7 @@ presentation = do
 
   ---- Lensの基本
   --stateSandbox $ do  
-  --  title "LensでHaskellをもっと格好良く！" 
+  --  title "LensでHaskellを\nもっと格好良く！" 
   --    $  "2015/5/30 Lens&Prism勉強会\n"
   --    ++ "by ちゅーん(@its_out_of_tune)"
   --slideTitle .= "自己紹介"
@@ -22,16 +22,16 @@ presentation = do
   --header "HaskellのLens" abountLens
 
   ---- Optic/Lens/Prism
-  --stateSandbox $ do  
-  --  slideFontSize .= Just 50
-  --  title "Opticから見たLens/Prism" 
-  --    $  "2015/5/30 Lens&Prism勉強会\n"
-  --    ++ "by ちゅーん(@its_out_of_tune)"
+  stateSandbox $ do  
+    slideFontSize .= Just 50
+    title "Opticから見たLens/Prism" 
+      $  "2015/5/30 Lens&Prism勉強会\n"
+      ++ "by ちゅーん(@its_out_of_tune)"
   --header "Lensの限界" lensLimit
   --header "Prismでパターンマッチ" prismPatternMuch
-  header "Prismの合成" prismCoposition
-  header "State上のPrism" prismWithState
-  --header "色々なFunctor" $ return ()
+  --header "Prismの合成" prismCoposition
+  --header "State上のPrism" prismWithState
+  header "色々なFunctor" anyFunctor
   --header "LensからOpticへ" $ return ()
   --header "Prismの仕組み" $ return ()
   --header "Opticで全体を見渡す" $ return ()
@@ -51,7 +51,8 @@ profile = do
     twinLeft
       ( listCont
         [ "野生のHaskller(27♂)"
-        , "東京都近郊に生息・休職中\n\n"
+        , "東京都近郊に生息"
+        , "休職中\n\n"
         , "クルージング(スケボー)"
         , "SOUND VOLTEX(音ゲー)"
         , "リトルノア（スマフォゲー）\n\n"
@@ -71,16 +72,13 @@ profile = do
       )
   list
     [  "Takahashi Monad：\n"
-    ++ "　高橋メソッドのスライド作成用に作った言語内DSL\n"
-    ++ "　大幅に拡張し複雑なスライドも作れるようになった。\n"
-    ++ "　本スライドもこれで作成しました"
+    ++ "　スライド作成用の言語内DSL\n"
+    ++ "　拡張済み／複雑なスライドも作れる\n"
+    ++ "　本スライドもこれで作成\n\n"
+    ++ "　GHC7.10非対応 。゜。゜（ノД｀）゜。゜。"
     ,  "Sarasvati：\n"
-    ++ "　オーディオインターフェイス\n"
-    ++ "　PortAudioのラッパーのラッパーとして開発中\n"
-    ,  "Arare：\n"
-    ++ "　Sarasvatiをベースに、\n"
-    ++ "　HaskellのDTM環境を作ろうとごにょごにょ企画中。\n"
-    ++ "　DSLベースを想定しているので多分DAWにはならない。"
+    ++ "　開発中のオーディオインターフェイス\n"
+    ++ "　PortAudioのラッパーのラッパー"
     ]
 
 --------
@@ -88,22 +86,25 @@ profile = do
 
 abountLens :: Taka ()
 abountLens = do
-  par
-    $  "2013/3/31 ekmett勉強会で、\n"
-    ++ "Haskellのlensについて発表しました。\n\n"
-    ++ "基本的な使い方から\n"
-    ++ "動作原理まで網羅的に紹介しています。\n\n"
-    ++ "まずは、当時のスライドを使ってLensの説明を行います。"
+  twinBottom
+    ( parCont
+      $  "2013/3/31 ekmett勉強会で、\n"
+      ++ "Haskellのlensについて発表。\n\n"
+    )
+    ( listCont
+      [ "基本的な使い方"
+      , "基本的な仕組みと実装"
+      , "簡単な応用等"
+      ]
+    )
   taka "旧スライドでの発表"
-  par
-    $  "いかかでしょうか、HaskellのLensのアプローチは\n"
-    ++ "Scalaのそれとはずいぶん違う事に気がついたと思います。\n"
-    ++ "Haskellだからこそ実現可能なこのLensの実装スタイルは\n"
-    ++ "考案者の名前を取ってvan Laarhoven lensと呼ばれるそうです。\n\n"
-    ++ "ekmettによるlens実装は、van Laarhoven lenをさらに拡張し、\n"
-    ++ "使いやすくまとめたものになっています。\n\n"
-    ++ "本日は、このlensパッケージを掘り下げて解説し、\n"
-    ++ "Laarhovenスタイルの優れた特徴をお伝えできればと思います"
+  list
+    [ "ScalaのLensと実装が違う？\n 　　-> van Laarhoven lens\n\n"
+    , "考案者 van Laarhovenがブログ記事に"
+    , "Russell O'Connor証明等を論文に\n\n"
+    , "ekmett/lensはvan Laarhoven lensの拡張"
+    , "Haskellのlensはもっとすごいぞっ！"
+    ]
 
 --------
 -- lensLimit
@@ -113,7 +114,7 @@ lensLimit = do
   twinBottom
     ( parCont 
       $  "Lensアクセッサを利用すれば、\n"
-      ++ "次のようにして複雑な構造へアクセスする事が出来るのでした。"
+      ++ "複雑な構造へアクセスする事が出来る"
     )
     ( codeCont
       $  "(1, (2, 3, (4, 999)), 6)^._2._3._2 -- 999\n\n"
@@ -121,18 +122,17 @@ lensLimit = do
     )
   vertical 
     [ parCont 
-      $  "それでは、次のfooような構造から\n"
-      ++ "\"999\"を操作したい場合はどうすれば良いでしょう？\n\n"
-      ++ "もちろん、barのような場合も考慮しなくてはいけません。"
+      $  "fooような構造から\"999\"を操作したい場合は？\n"
+      ++ "barのような場合は？"
     , codeCont
-      $  "let foo\n"
-      ++ "  = (1, Left (999, 3), 4) :: (Int, Either (Int, Int) String, Int)\n\n"
-      ++ "let bar\n"
-      ++ "  = (1, Right \"Test\", 4) :: (Int, Either (Int, Int) String, Int)"
+      $  "let foo = (1, Left (999, 3), 4) \n"
+      ++ "    :: (Int, Either (Int, Int) String, Int)\n\n"
+      ++ "let bar = (1, Right \"Test\", 4) \n"
+      ++ "    :: (Int, Either (Int, Int) String, Int)"
     ]
   twinBottom
     ( parCont 
-      $  "まずは値をGetする方法を考えてみます。"
+      $  "頑張ってlensだけでGetする\n"
     )
     ( codeCont
       $  "print $ case foo^._2 of \n"
@@ -145,7 +145,7 @@ lensLimit = do
   taka "(´ﾟдﾟ｀)ｴｰ"
   twinBottom
     ( parCont 
-      $  "Setしたい場合はどうすれば良いでしょうか？"
+      $  "頑張ってlensだけでSetする"
     )
     ( codeCont
       $  "(_2%~(\\case \n"
@@ -156,6 +156,12 @@ lensLimit = do
       ++ "  Right x -> Right x)) bar\n"
     )
   taka "｡･ﾟ･(ﾉ∀`)･ﾟ･｡"
+  stateSandbox $ do
+    contentsOption.blockFontSize .= Just 80
+    par
+      $  "lensは便利だけど\n"
+      ++ "直和型が混ざると使えない\n\n"
+      ++ "　-> そこでPrimsを使おう！"
 
 --------
 -- prismPatternMuch
@@ -164,10 +170,9 @@ prismPatternMuch :: Taka ()
 prismPatternMuch = do
   vertical
     [ parCont
-      $  "LensのアクセッサにはEitherやMaybeのような直和型、\n"
-      ++ "つまりパターンマッチが必要な型に対する、\n"
-      ++ "アクセス能力がありません。\n\n"
-      ++ "このような場合、Prismという型を持つアクセッサを使います。"
+      $  "Lensは直和型(Maybe, Either等)には使えない。\n\n"
+      ++ "このような場合、\n"
+      ++ "Prismという型を持つアクセッサを使う。"
     , codeCont
       $  "type Prism s t a b = ... \n\n"
       ++ "_Left :: Prism (Either a c) (Either b c) a b\n"
@@ -175,9 +180,9 @@ prismPatternMuch = do
     ]
   vertical
     [ parCont 
-      $  "Prismのアクセッサから値を取得するには、(^?)演算子を使います。\n\n"
-      ++ "この演算子はMaybe型を返し、値が取得できなかった場合、\n"
-      ++ "結果はNothingとなります。"
+      $  "Prismを使った値の取得には(^?)を使う。\n\n"
+      ++ "Maybe型を返し、値が取得できなかった場合、\n"
+      ++ "結果はNothingとなる。"
     , codeCont
       $  " let left5 = Left 5 :: Either Int String\n"
       ++ " let rightHoge = Right \"Hoge\" :: Either Int String\n\n"
@@ -188,19 +193,18 @@ prismPatternMuch = do
     ]
   vertical
     [ parCont 
-      $  "また、Prismはパターンにマッチした場合のみ値を変更可能な、\n"
-      ++ "Setterとして機能します。"
+      $  "Prismはマッチした場合のみ変更可能な、\n"
+      ++ "Setterとして機能する。"
     , codeCont
       $  "left5&_Left.~999      -- Left 999\n\n"
       ++ "rightHoge&_Left.~999  -- Right \"Hoge\""
     , parCont
-      $  "使い方はLensの場合と一緒です。"
+      $  "使い方はLensと同様"
     ]
   vertical
     [ parCont 
-      $  "さらに、詳細はまた後ほど説明しますが、取得したい値が、\n"
-      ++ "Monoidの場合に限り、(^.)演算子による値の取得が可能です。\n\n"
-      ++ "もしパターンマッチ出来なかった場合の値はmemptyになります。"
+      $  "取得したい値がMonoidの場合に限り、(^.)が使える。\n"
+      ++ "マッチ出来なかった場合の値はmemptyになる。"
     , codeCont
       $  "Just \"hoge\"^._Just                  -- OK \"hoge\"\n"
       ++ "Just 114514^._Just                  -- NG\n"
@@ -210,9 +214,9 @@ prismPatternMuch = do
   twinTop
     ( parCont
       $  "Prismはre関数を使う事によって、\n"
-      ++ "逆にデータコンストラクタを被せるアクセッサになります。\n"
-      ++ "この場合、値をSetする事は出来ません。\n\n"
-      ++ "同様の事は、to関数を使っても実現出来ますね。"
+      ++ "データコンストラクタを被せるアクセッサになる。\n"
+      ++ "値をSetする事は出来ない。\n\n"
+      ++ "同様の事は、to関数を使っても実現可能。"
     )
     ( codeCont
       $  "print $ 100^.re _Just -- Just 100\n\n"
@@ -221,13 +225,13 @@ prismPatternMuch = do
   twinBottom
     ( parCont
       $  "makePrisms関数を使えば、\n"
-      ++ "自分で定義した型に対してPrismアクセッサを作成できます。\n"
+      ++ "独自の型に対してPrismアクセッサを作成できる。\n"
     )
     ( codeCont
       $  "data Foo a b = Hoge a | Piyo b | Fuga String \n"
       ++ "  deriving (Show, Read, Eq, Ord)\n\n"
       ++ "makePrisms ''Foo\n\n"
-      ++ "------\n\n"
+      ++ "------ 以下のPrismが生成される ------\n\n"
       ++ "_Hoge :: Prism (Foo a c) (Foo b c) a b\n"
       ++ "_Piyo :: Prism (Foo c a) (Foo c b) a b\n"
       ++ "_Fuga :: Prism (Foo a b) (Foo a b) String String\n\n"
@@ -240,7 +244,8 @@ prismCoposition :: Taka ()
 prismCoposition = do
   twinBottom
     ( parCont 
-      $  "PrismもLensと同様、関数合成によって合成出来ます。"
+      $  "PrismもLensと同様、"
+      ++ "関数合成によって合成可能。"
     )
     ( codeCont
       $  "let justLeft = Just (Left 5) :: Maybe (Either Int String)\n"
@@ -252,8 +257,8 @@ prismCoposition = do
     )
   twinBottom
     ( parCont 
-      $  "仮に、PrismとLensを組み合わせて合成しても、\n"
-      ++ "もちろんアクセッサとしても利用できます。"
+      $  "当然、PrismとLensを組み合わせても、\n"
+      ++ "アクセッサとして利用可能。"
     )
     ( codeCont
       $  "let foo = (1, Left (999, 3), 4) \n"
@@ -267,9 +272,8 @@ prismCoposition = do
     )
   vertical
     [ parCont
-      $  "一度でもLensと合成したアクセッサに対して、\n"
-      ++ "re関数を使う事は出来ませんが、Prism同士を合成したアクセッサは、\n"
-      ++ "re関数に適用出来ます。"
+      $  "Lensと合成した後はre関数が使えない。\n"
+      ++ "合成したのがPrism同士ならば問題ない。"
     , codeCont
       $  "  re (_Just._1)                 -- NG\n\n"
       ++ "  (100^.re (_Just._Left) \n"
@@ -277,9 +281,9 @@ prismCoposition = do
     ]
   vertical
     [ parCont
-      $  "re関数を使って作成したPrismアクセッサを合成したものと、\n"
-      ++ "Prismアクセッサを合成したものをre関数に適用した場合では、\n"
-      ++ "結果の構造が逆になるため、注意してください。"
+      $  "Prismの合成 -> re関数\n"
+      ++ "Prismにre関数 -> 合成\n\n"
+      ++ "では、結果の構造が逆になるため注意。"
     , codeCont
       $  "print $ (100^.re _Just.re _Left\n"
       ++ "  :: Either (Maybe Int) Int)  -- Left (Just 100) \n\n"
@@ -287,12 +291,134 @@ prismCoposition = do
       ++ "  :: Maybe (Either Int Int))  -- Just (Left 100)"
     ]
 
+--------
+-- prismWithState
+
+prismWithState :: Taka ()
+prismWithState = do
+  vertical
+    [ parCont
+      $  "Lens同様、\n"
+      ++ "MonadStateインスタンス上で状態へのSetが可能。\n"
+    , codeCont
+      $  "sample :: State (Int, Bool, Maybe (Int, String)) ()\n"
+      ++ "sample = do\n"
+      ++ "  _1 .= 100\n"
+      ++ "  _3._Just._2 .= \"Hello, Prism!\"\n"
+      ++ "  return ()"
+    ]
+  vertical
+    [ parCont
+      $  "取り出したい値がモノイドならば\n"
+      ++ "use関数を用いた値のGetが出来る。"
+    , codeCont
+      $  "sample :: State (Int, Bool, Maybe (Int, String)) String\n"
+      ++ "sample = do\n"
+      ++ "  str <- use $ _3._Just._2\n"
+      ++ "  return str"
+    ]
+  vertical
+    [ parCont
+      $  "モノイドでない場合はpreuse関数を使う。"
+    , codeCont
+      $  "sampleState2 :: State (Int, Bool, Maybe (Int, String)) (Maybe Int)\n"
+      ++ "sampleState2 = do\n"
+      ++ "  int <- preuse $ _3._Just._1\n"
+      ++ "  return int"
+    ]
+  vertical
+    [ parCont
+      $  "取得したい値がIntならば次のようにしても良い。"
+    , codeCont
+      $  "sample :: State (Int, Bool, Maybe (Int, String)) (Sum Int)\n"
+      ++ "sample = do\n"
+      ++ "  int <- use $ _3._Just._1.to Sum\n"
+      ++ "  return int"
+    ]
+
+--------
+-- anyFunctor
+
+anyFunctor :: Taka ()
+anyFunctor = do
+  vertical
+    [ parCont
+      $  "Functorのイメージ：\n"
+      ++ "　実用上の認識に囚われない方が良い。"
+    , parCont2
+      $  "Functorの図式"
+    ]
+  vertical
+    [ parCont
+      $  "持ち上げ先の矢印を逆にする"
+    , parCont2
+      $  "Contravariantの図式"
+    ]
+  vertical
+    [ parCont
+      $  "FunctorとContravariant"
+    , codeCont
+      $  "class Functor f where\n"
+      ++ "  fmap      :: (a -> b) -> f a -> f b\n\n"
+      ++ "class Contravariant f where\n"
+      ++ "  contramap :: (a -> b) -> f b -> f a\n"
+    ]
+  vertical
+    [ takaCont
+      $  "シンキング・タイム(10秒)"
+    , parCont
+      $  "Contravariantになるような\n"
+      ++ "データ構造を考えてみよう！"
+    ]
+  vertical
+    [ parCont
+      $  "例１：関数"
+    , codeCont
+      $  "newtype Op b a = Op { runOp :: a -> b }\n\n"
+      ++ "instance Contravariant (Op r) where\n"
+      ++ "  contramap f (Op g) = Op $ g . f"
+    ]
+  vertical
+    [ parCont
+      $  "例２：Const"
+    , codeCont
+      $  "newtype Const a b = Const { getConst :: a }\n\n"
+      ++ "instance Contravariant (Const r) where\n"
+      ++ "  contramap _ = Const . getConst\n"
+    ]
+  vertical
+    [ parCont
+      $  "２つの関数を一つに束ねる"
+    , parCont2
+      $  "Bifunctorの図式"
+    ]
+  vertical
+    [ parCont
+      $  "Bifunctor"
+    , codeCont
+      $  "class Bifunctor f where\n"
+      ++ "  bimap :: (a -> b) -> (c -> d) -> f a c -> f b d\n\n"
+      ++ "  first :: (a -> b) -> f a c -> f b c\n"
+      ++ "  first f = bimap f id\n\n"
+      ++ "  second :: (b -> c) -> f a b -> f a c\n"
+      ++ "  second f = bimap id f"
+    ]
+  vertical
+    [ parCont
+      $  "Bifunctorの例：タプル、Const 等"
+    , codeCont
+      $  "instance Bifunctor (,)  where\n"
+      ++ "  bimap f g (x, y) = (f x, g y)\n\n"
+      ++ "instance Bifunctor Const  where\n"
+      ++ "  bimap f _ (Const x) = Const $ f x"
+    ]
+
 -------------------------------------------------
 -- helper
 
 setOptions :: Taka ()
 setOptions = do
-  slideFontSize .= Just 40
+  slideFontSize .= Just 50
   -- title
   titleOption.bgColor .= Just (Color 100 100 100)
   titleOption.fontColor .= Just (Color 255 255 255)
